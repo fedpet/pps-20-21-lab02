39f41a5db03db36d3d6c88c2312c1d41a6b1120b
fedpet <federico.pettinari2@studio.unibo.it>
2021-03-08 12:45:08 +0100

    Shapes impl

--- src/u02/Shapes.scala
+++ src/u02/Shapes.scala
@@ -1,5 +1,7 @@
 package u02
 
+import u02.Shapes.Shape.{Circle, Rectangle, Square}
+
 object Shapes {
   sealed trait Shape
   object Shape {
@@ -8,7 +10,15 @@ object Shapes {
     case class Square(size:Double) extends Shape
   }
 
-  def perimeter(s:Shape):Double = 0
+  def perimeter(s:Shape):Double = s match {
+    case Rectangle(width, height) => 2 * (width + height)
+    case Square(size) => 4 * size
+    case Circle(radius) => 2 * Math.PI * radius
+  }
 
-  def area(s:Shape):Double = 0
+  def area(s:Shape):Double = s match {
+    case Rectangle(width, height) => width * height
+    case Square(size) => size * size
+    case Circle(radius) => Math.PI * radius * radius
+  }
 }
