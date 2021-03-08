d769c04f31087e0d852b0d3f84b92df424a21c8e
fedpet <federico.pettinari2@studio.unibo.it>
2021-03-08 12:20:19 +0100

    FibonacciTest

--- test/u02/FibonacciTest.scala
+++ test/u02/FibonacciTest.scala
@@ -2,13 +2,14 @@ package u02
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import u02.Hello.fib
 
 class FibonacciTest {
   @Test def testFib() {
     assertEquals(0, fib(0))
     assertEquals(1, fib(1))
-    assertEquals(2, fib(1))
-    assertEquals(3, fib(2))
-    assertEquals(4, fib(3))
+    assertEquals(1, fib(2))
+    assertEquals(2, fib(3))
+    assertEquals(3, fib(4))
   }
 }
