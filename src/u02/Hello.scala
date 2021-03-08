ff5f9672f1ab1b7bd400f5018ffe89cd892c674f
fedpet <federico.pettinari2@studio.unibo.it>
2021-03-08 11:42:58 +0100

    NotEmpty impl

--- src/u02/Hello.scala
+++ src/u02/Hello.scala
@@ -3,5 +3,7 @@ package u02
 object Hello extends App {
   def parity(x: Int) = if (x % 2 == 0) "even" else "odd"
 
+  def neg(predicate: String => Boolean): String => Boolean = !predicate(_)
+
   println("Hello, Scala")
 }
