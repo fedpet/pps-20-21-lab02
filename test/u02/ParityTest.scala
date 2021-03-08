a2831e824332f7df5e0cc973201c8abcf99928e0
fedpet <federico.pettinari2@studio.unibo.it>
2021-03-08 13:31:38 +0100

    Refactor

--- test/u02/ParityTest.scala
+++ test/u02/ParityTest.scala
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test
 import u02.Hello.parity
 
 class ParityTest {
-  @Test def test() {
+  @Test def testParity() {
     assertEquals("even", parity(0))
     assertEquals("even", parity(-4))
     assertEquals("odd", parity(3))
