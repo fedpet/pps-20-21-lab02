737af793102f9bb168096c17535fe80187e707ca
fedpet <federico.pettinari2@studio.unibo.it>
2021-03-08 11:44:00 +0100

    Make neg generic

--- src/u02/Hello.scala
+++ src/u02/Hello.scala
@@ -3,7 +3,7 @@ package u02
 object Hello extends App {
   def parity(x: Int) = if (x % 2 == 0) "even" else "odd"
 
-  def neg(predicate: String => Boolean): String => Boolean = !predicate(_)
+  def neg[T](predicate: T => Boolean): T => Boolean = !predicate(_)
 
   println("Hello, Scala")
 }
