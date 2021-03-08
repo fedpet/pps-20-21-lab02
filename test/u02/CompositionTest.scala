92aa00117772b088109e15d985667876524f2b3b
fedpet <federico.pettinari2@studio.unibo.it>
2021-03-08 12:09:43 +0100

    Composition generic test

--- test/u02/CompositionTest.scala
+++ test/u02/CompositionTest.scala
@@ -6,9 +6,9 @@ import u02.Hello.compose
 
 class CompositionTest {
   @Test def testComposition() {
-    assertEquals(0, compose(_ * 2, _ - 1)(1))
-    assertEquals(2, compose(_ * 2, _ - 1)(2))
-    assertEquals(6, compose(_ * 2, _ - 1)(4))
-    assertEquals(9, compose(_ - 1, _ * 2)(5))
+    assertEquals(0, compose[Int, Int, Int](_ * 2, _ - 1)(1))
+    assertEquals(2, compose[Int, Int, Int](_ * 2, _ - 1)(2))
+    assertEquals(6, compose[Int, Int, Int](_ * 2, _ - 1)(4))
+    assertEquals(9, compose[Int, Int, Int](_ - 1, _ * 2)(5))
   }
 }
