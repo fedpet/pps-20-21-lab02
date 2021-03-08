e957e0e3b326fb5ae3d61be6908a3ca1b4438b2b
fedpet <federico.pettinari2@studio.unibo.it>
2021-03-08 12:02:01 +0100

    CompositionTest

---
+++ test/u02/CompositionTest.scala
@@ -0,0 +1,14 @@
+package u02
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class CompositionTest {
+  @Test def testComposition() {
+    val f: Int => Int = _ * 2
+    val g: Int => Int = _ - 1
+    assertEquals(0, compose(f, g)(1))
+    assertEquals(2, compose(f, g)(2))
+    assertEquals(6, compose(f, g)(4))
+  }
+}
