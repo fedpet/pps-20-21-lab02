4695f92a671ec40fc7a844a414fca5c0416c3832
fedpet <federico.pettinari2@studio.unibo.it>
2021-03-08 11:57:32 +0100

    Currying impl

--- src/u02/Hello.scala
+++ src/u02/Hello.scala
@@ -1,9 +1,12 @@
 package u02
 
-object Hello extends App {
-  def parity(x: Int) = if (x % 2 == 0) "even" else "odd"
+object Hello {
+  def parity(x: Int): String = if (x % 2 == 0) "even" else "odd"
 
   def neg[T](predicate: T => Boolean): T => Boolean = !predicate(_)
 
-  println("Hello, Scala")
+  val p1: Int => Int => Int => Boolean = (x:Int) => (y:Int) => (z:Int) => x <= y && y <= z
+  val p2: (Int, Int, Int) => Boolean = (x:Int, y:Int, z:Int) => x <= y && y <= z
+  def p3(x:Int): Int => Int => Boolean = (y:Int) => (z:Int) => x <= y && y <= z
+  def p4(x:Int, y:Int, z:Int): Boolean = x <= y && y <= z
 }
