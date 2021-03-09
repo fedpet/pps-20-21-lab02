23e01696774d2deb68ab58f1ce162042c7cd37c3
fedpet <federico.pettinari2@studio.unibo.it>
2021-03-09 18:54:04 +0100

    BTrees challenge

--- src/u02/BTrees.scala
+++ src/u02/BTrees.scala
@@ -8,11 +8,6 @@ object BTrees extends App {
     case class Leaf[A](value: A) extends Tree[A]
     case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
-    private def traverse[A, B](t: Tree[A])(branchMapper:(Tree[A], Tree[A]) => B)(leafMapper:A => B): B = t match {
-      case Branch(l, r) => branchMapper(l, r)
-      case Leaf(e) => leafMapper(e)
-    }
-
     def size[A](t: Tree[A]): Int = traverse(t)((l,r) => size(l) + size(r))(_ => 1)
 
     def find[A](t: Tree[A], elem: A): Boolean = traverse(t)((l,r) => find(l, elem) || find (r,elem))(_ == elem)
@@ -21,5 +16,10 @@ object BTrees extends App {
         case x if x == elem => 1
         case _ => 0
       }) // or better yet if(_ == elem) 1 else 0
+
+    private def traverse[A, B](t: Tree[A])(branchMapper:(Tree[A], Tree[A]) => B)(leafMapper:A => B): B = t match {
+      case Branch(l, r) => branchMapper(l, r)
+      case Leaf(e) => leafMapper(e)
+    }
   }
 }
